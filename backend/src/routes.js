135261f4baf40ebe48a2be3164877aee8117117e
Sushant Bhaiswar <96516472+SushantBhaiswar@users.noreply.github.com>
2023-02-18 13:57:11 +0530

    project updation

--- backend/src/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { LoginTeacher, RegisterTeacher } = require("./controllers/teacher")
-const router = express.Router()
-
-router.post("/register",RegisterTeacher)
-router.post("/login",LoginTeacher)
-module.exports = router
